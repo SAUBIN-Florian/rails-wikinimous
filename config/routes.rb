4c5cf1d4b17e0880ea8229086a60211455e223b7
floriansaubin <saubinflorian@gmail.com>
2021-03-02 16:44:25 +0100

    Challenge 3 WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: :article
+  post 'articles', to: 'articles#create'
+  delete 'articles/:id', to: 'articles#destroy'
 end
